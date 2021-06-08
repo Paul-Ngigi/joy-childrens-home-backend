95e90c544ea6e53737803a0522db84a4c57e6343
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-09 00:03:52 +0300

    add the child view

--- child/views.py
+++ child/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Child
+from .serializers import ChildSerializer
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class ChildView(APIView):
+    def get(self, request, format=None, *args, **kwargs):
+        pass
+
+    def post(self, request, format=None, *args, **kwargs):
+        pass
+
+    def put(self, request, pk, format=None, *args, **kwargs):
+        pass
+
+    def delete(self, request, pk, format=None, *args, **kwargs):
+        pass
