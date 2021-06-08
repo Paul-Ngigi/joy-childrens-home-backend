c916b3a9486219120a11e7c002fc062bc473154a
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-08 23:59:34 +0300

    update the serializer

--- child/serializers.py
+++ child/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Child
 
 
-class ChildSerializer(serializers.Serializer):
+class ChildSerializer(serializers.ModelSerializer):
     class Meta:
         model = Child
         fields = '__all__'
