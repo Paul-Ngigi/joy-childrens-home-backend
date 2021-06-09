5d8990aeb508038c6ee2a0af56da8c87c227bdfc
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-09 13:27:05 +0300

    add the CRUD methods

--- child/views.py
+++ child/views.py
@@ -1,53 +1,58 @@
-from django.db import models
 from django.shortcuts import render
 from django.http import Http404
-from rest_framework import serializers, status
 from .models import Child
 from .serializers import ChildSerializer
 
-from rest_framework.response import Response
+# rest dependencies import
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-# Create your views here.
-class ChildView(APIView):
-    serializer_class = ChildSerializer
+class AllChildren(APIView):
     model = Child
-    
-    def get_child(self, pk):
-        try:
-            return self.model.objects.get(pk=pk)
-        except self.model.DoesNotExist:
-            return Http404
-        
+    serializer = ChildSerializer
+
     def get(self, request, format=None, *args, **kwargs):
         children = self.model.objects.all()
-        serializers = self.serializer_class(children, many=True)
-        return Response(serializers.data)
+        serializer = self.serializer(children, many=True)
+
+        return Response(serializer.data)
 
     def post(self, request, format=None, *args, **kwargs):
-        serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk, format=None, *args, **kwargs):
-        child = self.get_child(pk)
-        serializers = self.serializer_class(child, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        else:
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None, *args, **kwargs):
-        child = self.get_child(pk)
-        child.delete()
-        
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer = self.serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class SingleChild(APIView):
-    def get(self, request, format=None, *args, **kwargs):
-        pass
+    model = Child
+    serializer = ChildSerializer
+
+    def get_object(self, request, pk, format=None, *args, **kwargs):
+        try:
+            return self.model.objects.get(pk=pk)
+        except self.model.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None, *args, **kwargs):
+        child = self.get_object(pk)
+        serializer = self.serializer(child)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None, *args, **kwargs):
+        child = self.get_object(pk)
+        serializer = self.serializer(child, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None, *args, **kwargs):
+        child = self.get_object(pk)
+        child.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
